f1df297742d02340965c3a4e1189df6cbbc4cb91
Rodrigo Colombo <alu0100630572@ull.edu.es>
2013-11-05 22:37:31 +0000

    Implementadas las pruebas de comprobacion del constructor

--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -10,4 +10,17 @@ describe Matriz do
     @m4 = Matriz.new([[4,1,6],[2,0,1]])
   end
 
+  describe "# Se crean las matrices del tamanio especificado e inicialidas" do
+    it "Se establecen correctamente las dimensiones" do
+      @m1.mCol.should == 2
+      @m1.nFil.should == 2
+      @m3.mCol.should == 3
+      @m3.nFil.should == 3
+    end
+    it "Se inicializa correctamente" do
+      @m1.to_s.should == "1 3 \n2 0 \n"
+      @m2.to_s.should == "2 1 \n0 1 \n"
+      @m3.to_s.should == "2 0 1 \n3 0 0 \n5 1 1 \n"
+    end
+  end
 end
