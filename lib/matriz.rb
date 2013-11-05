40b5a4e4a793f8a1f457b608e0f1f9cb6f3fb8b4
Oliver Valido <alu0100598148@ull.edu.es>
2013-11-05 23:31:37 +0000

    Implementados los métodos de suma, resta y multiplicación de matrices, pruebas de dichos métodos superadas correctamente

--- lib/matriz.rb
+++ lib/matriz.rb
@@ -20,7 +20,8 @@ class Matriz
     end
     matString
   end
-
+  
+  # Metodo para calcular el determinante de una matriz
   def det
     if (@nFil == @mCol)
     det = @matriz[0][0]
@@ -40,6 +41,7 @@ class Matriz
     det
   end
 
+  # Método apra calcular la traspuesta de una matriz
   def tras
     result = Array.new
       for i in 0...@nFil do
@@ -57,13 +59,70 @@ class Matriz
     end
     aux
   end
-
+  
+  # Metodo para multiplicar una matriz por un escalar
   def x(escalar)
     aux = Matriz.new(@matriz)
     for i in 0...@nFil do
       for j in 0...@mCol do
         aux.matriz[i][j] = @matriz[i][j] * escalar
       end
+    end
+    aux
+  end
+
+  #Metodo apra sumar dos matrices
+  def +(mat)
+    if (mat.nFil == @nFil && mat.mCol == @mCol)
+      aux = Matriz.new(@matriz)
+      for i in 0...@nFil do
+        for j in 0...@mCol do
+          aux.matriz[i][j] = @matriz[i][j] + mat.matriz[i][j]
+        end
+      end
+    else
+      aux = 0
+    end
+    aux
+  end
+
+  # Metodo para restar dos matrices
+  def -(mat)
+    if (mat.nFil == @nFil && mat.mCol == @mCol)
+      aux = Matriz.new(@matriz)
+      for i in 0...@nFil do
+        for j in 0...@mCol do
+          aux.matriz[i][j] = @matriz[i][j] - mat.matriz[i][j]
+        end
+      end
+    else
+      aux = 0
+    end
+    aux
+  end
+
+  # Metodo para multiplicar dos matrices
+  def *(mat)
+    if (@mCol == mat.nFil)
+      result = Array.new
+      for i in 0...@nFil do
+        result[i] = Array.new
+        for j in 0...mat.mCol do
+          result[i][j] = 0
+        end
+      end
+
+      aux = Matriz.new(result)
+
+      for i in 0...@nFil do
+        for j in 0...mat.mCol do
+          for z in 0...@mCol do
+            aux.matriz[i][j] += @matriz[i][z] * mat.matriz[z][j]
+          end
+        end
+      end
+    else
+      aux = 0
     end
     aux
   end
