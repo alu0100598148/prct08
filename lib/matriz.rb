1781a488d72383adcbd9dbfb44e851f0ec454d69
Oliver Valido <alu0100598148@ull.edu.es>
2013-11-05 23:08:02 +0000

    Implementados los métodos para realizar el determinante y la traspuesta de un matriz, realiza las pruebas unarias correctamente

--- lib/matriz.rb
+++ lib/matriz.rb
@@ -21,4 +21,40 @@ class Matriz
     matString
   end
 
+  def det
+    if (@nFil == @mCol)
+    det = @matriz[0][0]
+    aux = Matriz.new(@matriz)
+    for k in 0...@nFil do
+      l = k+1
+      for i in l...@mCol do
+        for j in l...@mCol do
+          aux.matriz[i][j] = (aux.matriz[k][k] * aux.matriz[i][j] - aux.matriz[k][j] * aux.matriz[i][k])/ aux.matriz[k][k]
+        end
+      end
+      det = det * aux.matriz[k][k]
+    end
+    else
+    det = "ERROR, la matriz no es cuadrada"
+    end
+    det
+  end
+
+  def tras
+    result = Array.new
+      for i in 0...@nFil do
+        result[i] = Array.new
+        for j in 0...@mCol do
+          result[i][j] = 0
+        end
+      end
+
+    aux = Matriz.new(result)
+    for i in 0...@nFil do
+      for j in 0...@mCol do
+        aux.matriz[i][j] = @matriz[j][i]
+      end
+    end
+    aux
+  end
 end
